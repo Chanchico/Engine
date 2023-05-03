e9e16c0da20de54079ce9c7dff2f9d604d00bd05
Chanchico <juanfranciscotraversi@gmail.com>
2023-05-03 23:32:08 +0200

    initialisation d'une fenêtre

--- src/main/java/com/chanchico/test/Launcher.java
+++ src/main/java/com/chanchico/test/Launcher.java
@@ -1,9 +1,18 @@
-package com.chanchico;
+package com.chanchico.test;
 
+import com.chanchico.core.WindowsManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println(Version.getVersion());
+        WindowsManager windowsManager = new WindowsManager("Test", 800, 600, true);
+        windowsManager.init();
+
+        while(!windowsManager.windowShouldClose()){
+            windowsManager.update();
+        }
+
+        windowsManager.cleanup();
     }
 }
